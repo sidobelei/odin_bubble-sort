890e349baff9c232330becfd98b0772701fb2604
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-05-15 12:28:21 -0700

    Add bubble_sort.rb and pseduocode

---
+++ bubble_sort.rb
@@ -0,0 +1,7 @@
+def bubble_sort(array)
+    #keep track of 1st operand, 2nd operand, 
+    #iterate through array
+    #go through two indexes at a time account for length
+    #check 1st and second, then second and third
+    #shift up if left number is bigger
+end
