8ced325bf2bd00318320a91e78dbeb652ae379e1
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-05-16 20:41:22 -0700

    Add bubble_sort function
    This function will take an array and return a
    sorted array in ascending order using a bubble
    sort methodology.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,7 +1,39 @@
 def bubble_sort(array)
-    #keep track of 1st operand, 2nd operand, 
-    #iterate through array
-    #go through two indexes at a time account for length
-    #check 1st and second, then second and third
-    #shift up if left number is bigger
+    new_array = []
+    sorted = false
+    while sorted == false do
+        current_element = array[0]
+        next_element = 0
+        difference = 0
+        array.each_with_index do |element, index|
+            if index + 1 < array.length
+                next_element = array[index + 1]
+                difference = current_element - next_element
+                if difference >= 1
+                    new_array.push(next_element)
+                elsif difference <= 0
+                    new_array.push(current_element)
+                    current_element = next_element
+                end
+            else
+                new_array.push(current_element)
+            end
+        end
+        new_array.each_with_index do |element, index|
+            if new_array[index + 1] == nil
+                break
+            end
+            if element <= new_array[index + 1]
+                sorted = true
+            else
+                sorted = false
+                array = new_array
+                new_array = []
+                break
+            end
+        end
+    end
+    p new_array
 end
+
+bubble_sort([4,3,78,2,0,2])
